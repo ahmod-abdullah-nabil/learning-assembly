c9dc91859fa2f681aab05855d61ec73f423272c4
ahmod-abdullah-nabil <ahmodabdullah876@gmail.com>
2026-06-29 18:00:00 +0800

    Fixed issue with program-2 Chapter 8

--- Homework-8/2.asm
+++ Homework-8/2.asm
@@ -1,28 +1,27 @@
-.486
+        .486 ;Please write a complete assembly program. Sum 1+2+3+….+200, and save the result to AX.
 STACK_ SEGMENT STACK 'STACK' USE16
-           DB 128 DUP (?)
+        DB 128 DUP ( ? )
 STACK_ ENDS
-
+        
 CODE SEGMENT USE16
-            ASSUME CS:CODE, SS:STACK_
-
-    BEG:
-            MOV    AX, STACK_
-            MOV    SS, AX
-            MOV    SP, 128
-
-            MOV    AX, 0
-            MOV    CX, 200
-            MOV    BX, 1
-
-    LSTART:
-            ADD    AX, BX
-            INC    BX
-            LOOP   LSTART
-    ; AX = 20100
-
-            MOV    AH, 4CH
-            INT    21H
+        ASSUME CS : CODE, SS : STACK_
+        
+BEG:
+        MOV    AX, STACK_
+        MOV    SS, AX
+        MOV    SP, 128
+        
+        MOV    AX, 0
+        MOV    CX, 200
+        MOV    BX, 1
+        
+LSTART:
+        ADD    AX, BX
+        INC    BX
+        LOOP   LSTART
+EXIT:
+        MOV    AH, 4CH
+        INT    21H
 
 CODE ENDS
-END BEG
+        END    BEG
