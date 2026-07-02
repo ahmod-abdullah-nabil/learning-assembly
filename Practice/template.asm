97b4881546601a89a9db0a6eaf1b2961721d421a
ahmod-abdullah-nabil <ahmodabdullah876@gmail.com>
2026-07-02 13:27:26 +0800

    assembly template for all the codes

---
+++ Practice/template.asm
@@ -0,0 +1,21 @@
+    .486
+    
+DATA SEGMENT USE16
+DATA ENDS
+    
+STACK_ SEGMENT STACK 'STACK' USE16
+    DB 128 DUP ( ? )
+STACK_ ENDS
+    
+    
+CODE SEGMENT USE16
+    ASSUME CS : CODE, DS : DATA, SS : STACK_
+BEG:
+    
+    
+EXIT:
+    MOV    AH, 4CH
+    INT    21H
+
+CODE ENDS
+    END    BEG
